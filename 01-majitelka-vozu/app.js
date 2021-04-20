90858e20a6e4b351f2c4e51e6098e829cfb768c3
Martina Deutschová <deutschova.martina@gmail.com>
2021-04-20 18:52:00 +0200

    [FEATURE] car owner object added + UI propagation

--- 01-majitelka-vozu/app.js
+++ 01-majitelka-vozu/app.js
@@ -1,10 +1,26 @@
 // Vytvoř objekt auto, který bude mít vlastnosti: znacka, barva, rokVyroby,..
-
+let auto = {
+    znacka: 'Ford', 
+    barva: 'modrá',
+    rokVyroby: 2015,
+    spz: '1B2 1234'
+}
 
 // Přidej novou vlastnost (majitel a prirad ji svoje jmeno jako hodnotu)
+auto.majitel = {}
+auto.majitel.jmeno = 'Jana'
+auto.majitel.prijmeni = 'Czechita'
 
 
 // Přepiš značku na Škoda
-
+auto['znacka'] = 'Škoda'
 
 // Vytvor tri promenne majitelka, znacka a spz a vloz do techto elementu text z objektu auto.
+const majitelka = auto.majitel.jmeno + ' ' + auto.majitel.prijmeni
+document.querySelector('#majitelka').innerHTML = majitelka
+
+const znacka = auto.znacka
+document.querySelector('#znacka').innerHTML = znacka
+
+const spz = auto.spz
+document.querySelector('#spz').innerHTML = spz
